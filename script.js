bd6f75770b69c40f1d11d53c32827e8bcfba79f2
nickrmeier <nickrmeier@gmaill.com>
2020-04-14 15:07:27 -0400

    array is showing correct number, and correct values for the light pattern

--- script.js
+++ script.js
@@ -45,17 +45,10 @@ let changeBackD = () => {
     buttonFour.style.backgroundColor = "rgb(91, 134, 147)";
 }
 
-
 // EACH BUTTON IN AN ARRAY
 let gameArray = [];
 gameArray.push(buttonOne, buttonTwo, buttonThree, buttonFour);
 console.log(gameArray);
-
-
-//  RANDOM GENERATOR 1
-// let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
-// console.log(randomItem)
-
 
 // TURN COUNTER
 const turnCounter = document.querySelector('.turncount');
@@ -65,164 +58,226 @@ const compArray = [];
 
 // START BUTTON/COMPUTER TURN 1
 // click FUNCTION (this is also reset)
-const startButton = document.querySelector('.play');
+let startButton = document.querySelector('.play');
 startButton.addEventListener('click', chooseRandomItem);
+
+
 function chooseRandomItem(event) {
-    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
+    turnCounter.innerHTML = 'Round 1';
     startButton.style.backgroundColor = "grey";
-    // this clicks the item chosen by the random generator
-    // randomItem.click();
+    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
     randomItem.style.backgroundColor = "yellow";
     setTimeout(function () {
         randomItem.style.backgroundColor = randomItem.dataset.color
     }, 500);
-    turnCounter.innerHTML = 'Round 1';
     compArray.push(randomItem);
 
-}
-
-
-console.log(compArray)
-// // computer goes first computer 
-// // computer picks number, then flashes light 
-// // the pattern goes into an array
-
-
-
-// USER CLICK (turn one)
-// let userArray = [];
-// console.log(userArray);
-
-document.addEventListener('click', function (event) {
-    if (!event.target.matches('.button')) return;
-    // event.preventDefault();
-    var userInput = (event.target);
-    // userArray.push(userInput);
-    // console.log(userArray);
-    // compArray.push(userInput)
-    console.log(userInput)
-    // if (userArray[0] == userArray[1]) {
-    turnTwo();
-    // };
-}, false);
-
-// document.removeEventListener('click', userInput);
-
-
-
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('.button')) return;
+        let userInput = (event.target);
+        console.log(userInput)
+        turnTwo();
+    }, false);
+    console.log(compArray)
+};
 
 // TURN TWO
 
-//  RANDOM GENERATOR 2
-let randomItemTwo = gameArray[Math.floor(Math.random() * gameArray.length)];
-
-function turnTwo() {
+function turnTwo(event) {
     turnCounter.innerHTML = 'Round 2';
     startButton.style.backgroundColor = "white";
-    // compArray.push(randomItemTwo);
-    chooseRandomItem();
-    console.log(compArray);
+    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
+    randomItem.style.backgroundColor = "yellow";
+    setTimeout(function () {
+        randomItem.style.backgroundColor = randomItem.dataset.color
+    }, 2000);
 
-    let userArrayTwo = [];
-
-    var listenerTwo = document.addEventListener('click', function (event) {
+    compArray.push(randomItem);
+    document.addEventListener('click', function (event) {
         if (!event.target.matches('.button')) return;
-        event.preventDefault();
-        var userInputTwo = (event.target);
-        userArrayTwo.push(userInputTwo);
-        console.log(userArrayTwo);
-        if (userArrayTwo[0] == compArray[1]) {
-            turnThree();
-        };
+        let userInput = (event.target);
+        console.log(userInput)
+        turnThree();
     }, false);
+    console.log(compArray)
+};
 
-    document.removeEventListener('click', listenerTwo);
-}
+// TURN THREE
 
-
-//  RANDOM GENERATOR 3
-let randomItemThree = gameArray[Math.floor(Math.random() * gameArray.length)];
-
-function turnThree() {
+function turnThree(event) {
     turnCounter.innerHTML = 'Round 3';
     startButton.style.backgroundColor = "white";
-    compArray.push(randomItemThree);
-    console.log(compArray);
+    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
+    randomItem.style.backgroundColor = "yellow";
+    setTimeout(function () {
+        randomItem.style.backgroundColor = randomItem.dataset.color
+    }, 4000);
+    console.log(compArray)
 
-    let userArrayThree = [];
-    console.log('turnThree running')
     document.addEventListener('click', function (event) {
         if (!event.target.matches('.button')) return;
-        event.preventDefault();
-        var userInputThree = (event.target);
-        userArrayThree.push(userInputThree);
-        console.log(userArrayThree);
-        if (userArrayThree[0] == compArray[3]) {
-            turnFour();
-        };
+        let userInput = (event.target);
+        console.log(userInput)
+        turnFour();
     }, false);
-
-    // document.removeEventListener('click', turnThree);
-
+    console.log(compArray)
 };
 
+// TURN FOUR
 
-//  RANDOM GENERATOR 4
-let randomItemFour = gameArray[Math.floor(Math.random() * gameArray.length)];
-
-function turnFour() {
+function turnFour(event) {
     turnCounter.innerHTML = 'Round 4';
     startButton.style.backgroundColor = "white";
-    compArray.push(randomItemFour);
-    console.log(compArray);
-
-    let userArrayFour = [];
+    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
+    randomItem.style.backgroundColor = "yellow";
+    setTimeout(function () {
+        randomItem.style.backgroundColor = randomItem.dataset.color
+    }, 5000);
+    console.log(compArray)
 
     document.addEventListener('click', function (event) {
         if (!event.target.matches('.button')) return;
-        event.preventDefault();
-        var userInputFour = (event.target);
-        userArrayFour.push(userInputFour);
-        console.log(userArrayFour);
-        if (userArrayFour[0] == compArray[6]) {
-            turnFive();
-        };
+        let userInput = (event.target);
+        console.log(userInput)
+        turnFive();
     }, false);
-
-    // document.removeEventListener('click', this);
-
+    console.log(compArray)
 };
 
+// TURN FIVE
 
 function showAlert() {
     alert('YOU WIN!');
 };
 
-//  RANDOM GENERATOR 5
-let randomItemFive = gameArray[Math.floor(Math.random() * gameArray.length)];
-
-function turnFive() {
+function turnFive(event) {
     turnCounter.innerHTML = 'Round 5';
     startButton.style.backgroundColor = "white";
-    compArray.push(randomItemFive);
-    console.log(compArray);
-
-    let userArrayFive = [];
+    let randomItem = gameArray[Math.floor(Math.random() * gameArray.length)];
+    randomItem.style.backgroundColor = "yellow";
+    setTimeout(function () {
+        randomItem.style.backgroundColor = randomItem.dataset.color
+    }, 8000);
+    console.log(compArray)
 
     document.addEventListener('click', function (event) {
         if (!event.target.matches('.button')) return;
-        event.preventDefault();
-        var userInputFive = (event.target);
-        userArrayFive.push(userInputFive);
-        console.log(userArrayFive);
-        if (userArrayFive[0] == compArray[11]) {
-            showAlert();
-        };
+        let userInput = (event.target);
+        console.log(userInput)
+        showAlert();
     }, false);
-
-    // document.removeEventListener('click', this);
-
+    console.log(compArray)
 };
+
+
+
+// function turnThree() {
+//     turnCounter.innerHTML = 'Round 3';
+//     startButton.style.backgroundColor = "white";
+//     chooseRandomItem();
+//     console.log(compArray);
+
+//     let userArrayThree = [];
+
+//     document.addEventListener('click', function (event) {
+//         if (!event.target.matches('.button')) return;
+//         var userInputThree = (event.target);
+//         userArrayThree.push(userInputThree);
+//         console.log(userArrayThree);
+//         if (userArrayThree[0] == compArray[1]) {
+//             turnFour();
+//         };
+//     }, false);
+// };
+
+
+
+
+
+
+// //  RANDOM GENERATOR 3
+// let randomItemThree = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+// function turnThree() {
+//     turnCounter.innerHTML = 'Round 3';
+//     startButton.style.backgroundColor = "white";
+//     compArray.push(randomItemThree);
+//     console.log(compArray);
+
+//     let userArrayThree = [];
+//     console.log('turnThree running')
+//     document.addEventListener('click', function (event) {
+//         if (!event.target.matches('.button')) return;
+//         event.preventDefault();
+//         var userInputThree = (event.target);
+//         userArrayThree.push(userInputThree);
+//         console.log(userArrayThree);
+//         if (userArrayThree[0] == compArray[3]) {
+//             turnFour();
+//         };
+//     }, false);
+
+//     // document.removeEventListener('click', turnThree);
+
+// };
+
+
+// //  RANDOM GENERATOR 4
+// let randomItemFour = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+// function turnFour() {
+//     turnCounter.innerHTML = 'Round 4';
+//     startButton.style.backgroundColor = "white";
+//     compArray.push(randomItemFour);
+//     console.log(compArray);
+
+//     let userArrayFour = [];
+
+//     document.addEventListener('click', function (event) {
+//         if (!event.target.matches('.button')) return;
+//         event.preventDefault();
+//         var userInputFour = (event.target);
+//         userArrayFour.push(userInputFour);
+//         console.log(userArrayFour);
+//         if (userArrayFour[0] == compArray[6]) {
+//             turnFive();
+//         };
+//     }, false);
+
+//     // document.removeEventListener('click', this);
+
+// };
+
+
+// function showAlert() {
+//     alert('YOU WIN!');
+// };
+
+// //  RANDOM GENERATOR 5
+// let randomItemFive = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+// function turnFive() {
+//     turnCounter.innerHTML = 'Round 5';
+//     startButton.style.backgroundColor = "white";
+//     compArray.push(randomItemFive);
+//     console.log(compArray);
+
+//     let userArrayFive = [];
+
+//     document.addEventListener('click', function (event) {
+//         if (!event.target.matches('.button')) return;
+//         event.preventDefault();
+//         var userInputFive = (event.target);
+//         userArrayFive.push(userInputFive);
+//         console.log(userArrayFive);
+//         if (userArrayFive[0] == compArray[11]) {
+//             showAlert();
+//         };
+//     }, false);
+
+//     // document.removeEventListener('click', this);
+
+// };
 
 
 
